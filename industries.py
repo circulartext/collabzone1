2003b1c0dc6aa6f7702080451c2be4e5128cd364
circulartext <106709424+circulartext@users.noreply.github.com>
2024-01-16 11:14:16 -0500

    Update industries.py

--- industries.py
+++ industries.py
@@ -1,1 +1,3 @@
 auto_industry = ["ejection molding", "wire assembly", "drivers", "sales", "consumer",]
+auto_indautomate = ["80%", "50%", "%5", "25%", "1%",]
+auto_indhuman = ["20%", "50%", "95%", "75%", "99%",]
